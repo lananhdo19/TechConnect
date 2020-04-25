378240920e1b7ea077990cf631213633738575cb
Holly Wikiera <59944490+hwikiera@users.noreply.github.com>
2020-04-25 16:25:37 -0400

    add listing

--- user-db.php
+++ user-db.php
@@ -3,17 +3,16 @@
 //this is for the database functions to be used on the homepage
 //it'll go to the table listing and get all the information
 //can also delete a listing from the database
-function add_listing($brand, $item_condition, $type, $description, $price, $image){
+function add_listing($brand, $item_condition, $type, $description, $price){
     global $db;
     //collon specifies a "fill in the blank"
-    $query = "INSERT INTO listing (brand, item_condition, type, description, price, image) VALUES (:item_id, :brand, :item_condition, :type, :description, :price, :image);";
+    $query = "INSERT INTO listing (brand, item_condition, type, description, price) VALUES (:item_id, :brand, :item_condition, :type, :description, :price);";
     $statement = $db->prepare($query);
     $statement->bindValue(':brand', $brand);
     $statement->bindValue(':item_condition', $item_condition);
     $statement->bindValue(':type', $type);
     $statement->bindValue(':description', $description);
     $statement->bindValue(':price', $price);
-    $statement->bindValue(':image', $image);
     $statement->execute();
     $statement->closeCursor();
 }
