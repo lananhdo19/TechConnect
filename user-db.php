faf84ad1f8160efa35405e97ae2ed814b267b7a9
Holly Wikiera <59944490+hwikiera@users.noreply.github.com>
2020-04-25 17:12:54 -0400

    lists

--- user-db.php
+++ user-db.php
@@ -16,4 +16,19 @@ function add_listing($brand, $item_condition, $type, $description, $price){
     $statement->execute();
     $statement->closeCursor();
 }
+function add_list($brand, $item_condition, $type, $description, $price, $username){
+    global $db;
+    //collon specifies a "fill in the blank"
+    $query = "INSERT INTO lists (item_id, username) VALUES ((SELECT item_id from listing where brand = :brand AND
+    item_condition = :item_condition AND type = :type AND description = :description AND price = :price), :username);";
+    $statement = $db->prepare($query);
+    $statement->bindValue(':brand', $brand);
+    $statement->bindValue(':item_condition', $item_condition);
+    $statement->bindValue(':type', $type);
+    $statement->bindValue(':description', $description);
+    $statement->bindValue(':price', $price);
+    $statement->bindValue(':username', $username);
+    $statement->execute();
+    $statement->closeCursor();
+}
 ?>
